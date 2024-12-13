f5a40777e478b2984d55b6eea918c4ff3504c839
qiqiyiyiguo <3370256643@qq.com>
2024-12-13 10:19:24 +0800

    goods page

--- 前端/scripts/goods.js
+++ 前端/scripts/goods.js
@@ -14,4 +14,33 @@ function clickAfter(who) {
     else if(who==='首页'){
         window.location.href = "./index.html"
     }
+    else if(who==='play'){
+        startMove();
+    }
 }
+
+
+
+function setAnimationDuration(element, duration) {
+    element.style.animationDuration = `${duration}s`;
+  }
+  const element = document.querySelector('.content');
+  setAnimationDuration(element, 22); 
+
+
+
+// 控制动画开始和暂停
+function startMove() {
+    let elem = document.querySelector('.content');
+
+    // 如果动画是暂停的，那么开始动画
+    if (elem.style['animationPlayState'] === 'paused' || elem.style['animationPlayState'] === '') {
+        elem.style['animationPlayState'] = 'running';
+        
+        // 在开始动画后2秒自动暂停动画
+        setTimeout(() => {
+            elem.style['animationPlayState'] = 'paused';
+        }, 2000); // 2秒后暂停动画
+    }
+}
+
