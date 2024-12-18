1a6c806b818982f2c674101e20d64bc0d0d7585a
YiQiuchen11 <3124637154@qq.com>
2024-12-18 15:08:41 +0800

    添加客服页面

---
+++ 前端/consult.js
@@ -0,0 +1,3 @@
+const messageForm = document.getElementById('messageForm');
+const messageInput = document.getElementById('messageInput');
+const chatMessages = document.getElementById('chatMessages');
